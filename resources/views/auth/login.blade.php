87a43c7189a209c2f14d3f012fb5a398a19377d7
Carlos Estifen Almario <carloselmax95@gmail.com>
2016-09-26 17:36:09 -0500

    se ajusto el login con materialize

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,80 +3,73 @@
 <html lang="en">
 <head>
     <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('plugins/bootstrap/css/bootstrap.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('plugins/Materialize/css/materialize.css')}}">
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="stylesheet" type="text/css" href="{{ asset('plugins/login/styles.css')}}">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    
 </head>
 <body>
-      <div class="row">
+       
+            <div class="row">
+                <div class="col s12 m4 l4 card-panel  centrar offset-s2 ">
+                    <form>
 
-     
-
-        <div class="col-md-4 col-md-offset-4 centrar">
-
-            <div class="panel panel-default">
-
-                <div class="panel-heading cabecera">Iniciar sesión</div>
-
-                <div class="panel-body">
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-
-                            <label for="email" class="col-md-4 control-label ">Correo</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control " name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="row">
+                            
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Contraseña</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control lab" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="email" type="email" class="validate">
+                                <label for="email">Correo</label> 
                             </div>
+                 
                         </div>
 
-        
+                        <div class="row">
+                             <div class="input-field col s12">
+                                <input id="email" type="password" class="validate">
+                                <label for="email">Contraseña</label> 
+                            </div>  
+                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary btn-lg btn-block">
-                                    <i class="fa fa-btn fa-sign-in"></i> Ingresar
+                        <div class="row">
+                            <div class="col input-field">
+                                <button class="btn waves-effect waves-light s2" type="submit" name="action">Ingresar
+                                    <i class="material-icons right">send</i>
                                 </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">¿olvidaste tu contraseña?</a>
+        
                             </div>
                         </div>
-                    </form>
 
+                        <div class="row">
+                            <div class=" input-field col s6  offset-s3">
+                                <p class="margin" >
+                                    <a href="www.google.com">¿olvido su contraseña?</a>
+                                </p>
+                            </div> 
+                        </div>
+
+                        
+
+                    </form>
                 </div>
             </div>
-        </div>
-    </div>
+        
+
+
+
+
+
+
 
 
     <script src="{{asset('plugins/jquery/jquery-3.1.0.js')}}"></script>
-    <script src="{{asset('plugins/bootstrap/js/bootstrap.js')}}"></script>
-</body>
+    <script src="{{asset('plugins/Materialize/js/materialize.js')}}"></script>
+    
+ </body>   
 </html>
 
   
