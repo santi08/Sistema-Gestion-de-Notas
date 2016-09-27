baa8d93620a6f8081f22da95b20957c998a96b28
Carlos Estifen Almario <carloselmax95@gmail.com>
2016-09-27 01:14:01 -0500

    login en un 75% falta terminar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,59 +4,62 @@
 <head>
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('plugins/Materialize/css/materialize.css')}}">
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="stylesheet" type="text/css" href="{{ asset('plugins/login/styles.css')}}">
     <meta charset="utf-8">
     
 </head>
 <body>
-       
-            <div class="row">
-                <div class="col s12 m4 l4 card-panel  centrar offset-s2 ">
-                    <form>
 
+       <div class="container">
+
+                <div class="row">
+                    <div class="">
+                    <div class="col s12 m10 l4 card-panel centrar offset-l4 z-depth-3  ">
                         <div class="row">
-                            
+                            <div class="col s12 input-field center">
+                                <img class="responsive-img " src="{{ asset('img/notes.png')}}">
+                            </div>
                         </div>
 
                         <div class="row">
-                            <div class="input-field col s12">
+                            <div class="col s12 input-field"><i class="material-icons prefix">account_circle</i>
                                 <input id="email" type="email" class="validate">
-                                <label for="email">Correo</label> 
-                            </div>
-                 
-                        </div>
-
-                        <div class="row">
-                             <div class="input-field col s12">
-                                <input id="email" type="password" class="validate">
-                                <label for="email">Contraseña</label> 
-                            </div>  
-                        </div>
-
-                        <div class="row">
-                            <div class="col input-field">
-                                <button class="btn waves-effect waves-light s2" type="submit" name="action">Ingresar
-                                    <i class="material-icons right">send</i>
-                                </button>
-        
+                                
+                                <label for="email">Correo</label>
                             </div>
                         </div>
 
                         <div class="row">
-                            <div class=" input-field col s6  offset-s3">
-                                <p class="margin" >
-                                    <a href="www.google.com">¿olvido su contraseña?</a>
+                            <div class="col s12 input-field ">
+                                <input id="password" type="password" class="validate ">
+                                <i class="material-icons">lock_outline</i>
+                                <label for="password ">Contraseña</label>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col s12 input-field offset-l3">
+                                <button class="btn waves-effect waves-light red darken-4" type="submit" name="action">Entrar
+                                <i class="material-icons left ">exit_to_app</i>
+                                </button>     
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col s12 input-field offset-l3">
+                                <p>
+                                    <a href="www.google.com">¿Olvidaste tu contraseña?</a>
                                 </p>
-                            </div> 
+                            </div>
                         </div>
 
-                        
+                    </div>
 
-                    </form>
+                    </div>
                 </div>
-            </div>
+        </div>
         
 
 
