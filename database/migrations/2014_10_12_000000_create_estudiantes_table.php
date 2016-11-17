f5588d7fd1731294e67588bd3700e5a99494873d
santi08 <santiaristi_1994@hotmail.com>
2016-11-17 17:23:15 -0500

    estudiantes

--- database/migrations/2014_10_12_000000_create_estudiantes_table.php
+++ database/migrations/2014_10_12_000000_create_estudiantes_table.php
@@ -19,11 +19,7 @@ class CreateEstudiantesTable extends Migration
             $table->string('primerApellido');
             $table->string('segundoApellido');
             $table->string('codigo');
-<<<<<<< HEAD
             $table->string('email')->nullable();
-=======
-            $table->string('email')->nullable()->unique();
->>>>>>> 2c633c36c1076e46e041da647f04215a2abd48be
             $table->string('password');
             $table->integer('id_programaAcademico')->unsigned();
             $table->boolean('estado')->nullable()->default('1');
