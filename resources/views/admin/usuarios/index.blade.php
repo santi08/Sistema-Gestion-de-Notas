9821d38ef60638a1e329fc5a1aa2eafc81e06096
Carlos Estifen Almario Galindez <carloselmax95@hotmail.com>
2016-12-13 12:05:37 -0500

    antes de actualizar repositorio

--- resources/views/admin/usuarios/index.blade.php
+++ resources/views/admin/usuarios/index.blade.php
@@ -75,27 +75,12 @@
                      </tr>
                   </thead>
                   <tbody>
-                     @foreach($estudiantes as $estudiante)
-                        <tr>
-                           <td> {{$estudiante->codigo}}</td>
-                           <td> {{ $estudiante->primerNombre}} {{$estudiante->segundoNombre}} {{$estudiante->primerApellido}}</td>
-                           <td> {{ $estudiante->email}}</td>
-                           <td> {{$estudiante->programaAcademico->NombrePrograma}}</td>
-                           <td>  
-                              <a onClick="abrirModalEditar({{$estudiante->id}})"  data-target='#editarEstudiante' class="btn-flat"><i class="material-icons orange-text text-darken-1">edit</i></a> 
-
-                              <a onclick="eliminar({{$estudiante->id}});" id="{{$estudiante->id}}" class="btn-flat "><i class="material-icons red-text">delete</i></aX>
-
-                              <a onClick="listarAsignaturas({{$estudiante->id}})" class="btn-flat tooltiped" data-position="bottom" data-delay="50" data-target='#listarAsignaturas' data-tooltip="asignaturas"><i class="material-icons blue-text text-darken-3">visibility</i></a>
-                           </td> 
-                        </tr>
-                     @endforeach
+                     
+                 
                   </tbody>
                </table>
 
-               <div class="center">
-                  {!! $estudiantes->render()!!}
-               </div>
+              
          </div>
 
       </div>
